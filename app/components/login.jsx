806ee6aefde6e16cf3ad138fb493d6b380b140e5
Justin Mendelson <jmendelson22@gmail.com>
2017-02-20 15:07:59 -0800

    small change to language

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -10,7 +10,7 @@ class Login extends React.Component {
           <img src="assets/img/cotton_candy.png"/>
           <span>ReferralCandy</span>
         </div>
-        <h4 className="sub-heading">Refer friends. Receive Rewards.</h4>
+        <h4 className="sub-heading">Refer friends. Be awesome.</h4>
         <form onSubmit={this.props.handleLogin}>
           <input className="email-input" placeholder="Your Email" type="text" name="email" value={this.props.email} onChange={this.props.updateEmail}/>
           <input className="email-submit" type="submit" value="Step Inside"/>
